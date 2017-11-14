a0b537f86959fd92305d484752026a602b554e50
Zach Visagie <zavisagie@gmail.com>
2017-11-14 19:58:12 +0200

    Fix dispatcher url-pattern and spring default login page

--- mvc/src/main/java/com/insclix/mvc/server/config/WebSecurityConfig.java
+++ mvc/src/main/java/com/insclix/mvc/server/config/WebSecurityConfig.java
@@ -24,7 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .formLogin()
                     .defaultSuccessUrl("/", true)
-                    .loginPage("/login")
                     .loginProcessingUrl("/login")
             .and()
                 .logout()
